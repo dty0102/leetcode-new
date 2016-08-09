51c609b74c89bb7686d7d35409b6d80b7433b7cb
Tianyi Ding <td324@cornell.edu>
2016-08-08 21:24:40 -0400

    Update reverse-string.java

--- 344.reverse-string/reverse-string.java
+++ 344.reverse-string/reverse-string.java
@@ -14,16 +14,18 @@ var reverseString = function(s) {
 };
 
 2. convert string to a char array, create two points to start and end, exchange the char, convert it back to string as result
-var reverseString = function(s) {
-    array = s.split();
-    start = 0;
-    end = array.length - 1;
-    while(start < end){
-        temp = array[start];
-        array[start] = array[end];
-        array[end] = temp;
-        start++;
-        end--;
+public class Solution {
+    public String reverseString(String s) {
+        char[] array = s.toCharArray();
+        int start = 0;
+        int end = s.length() - 1;
+        while (start<end){
+            char temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+            start++;
+            end--;
+        }
+        return new String(array);
     }
-    return array.join();
-};
+}
