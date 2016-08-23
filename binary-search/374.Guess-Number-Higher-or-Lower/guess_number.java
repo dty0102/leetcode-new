debb8d0c80f3f863585e946c22742ae8fcb2352d
Tianyi Ding <td324@cornell.edu>
2016-08-22 22:52:26 -0400

    Create guess_number.java

---
+++ binary-search/374.Guess-Number-Higher-or-Lower/guess_number.java
@@ -0,0 +1,19 @@
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int start = 1;
+        int end = n;
+        while(start < n){
+            int mid = start + (end - start)/2;
+            if (guess(mid) == 0){
+                return mid;
+            } 
+            else if(guess(mid) == 1){
+                start = mid + 1;
+            }
+            else{
+                end = mid;
+            }
+        }
+        return start;
+    }
+}
