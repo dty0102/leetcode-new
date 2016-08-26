f39cc106d8da1ea6d746e91123410de365f95b51
Tianyi Ding <td324@cornell.edu>
2016-08-25 22:44:27 -0400

    Update *intersection_of_two_arrays_ii.java

--- binary-search/350.Intersection-of-Two-Arrays-II/*intersection_of_two_arrays_ii.java
+++ binary-search/350.Intersection-of-Two-Arrays-II/*intersection_of_two_arrays_ii.java
@@ -65,26 +65,28 @@ public class Solution {
 2. Improvement of the hashmap methods
     public int[] intersection(int[] nums1, int[] nums2) {
         // Write your code here
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for(int i = 0; i < nums1.length; ++i) {
-            if (map.containsKey(nums1[i]))
-                map.put(nums1[i], map.get(nums1[i]) + 1); 
-            else
-                map.put(nums1[i], 1);
+        if(nums1 == null || nums2 == null){
+            return null;
         }
-
-        List<Integer> results = new ArrayList<Integer>();
-
-        for (int i = 0; i < nums2.length; ++i)
-            if (map.containsKey(nums2[i]) &&
-                map.get(nums2[i]) > 0) {
-                results.add(nums2[i]);
-                map.put(nums2[i], map.get(nums2[i]) - 1); 
+        HashMap<Integer,Integer> hash = new HashMap<Integer,Integer>();
+        for (int i = 0; i < nums1.length; i++){
+            if(hash.containsKey(nums1[i])){
+                hash.put(nums1[i],hash.get(nums1[i]) + 1);
             }
-
-        int result[] = new int[results.size()];
-        for(int i = 0; i < results.size(); ++i)
+            else{
+                hash.put(nums1[i],1);
+            }
+        }
+        List<Integer> results = new ArrayList<Integer>();
+        for (int j = 0; j < nums2.length; j++){
+            if(hash.containsKey(nums2[j]) && hash.get(nums2[j]) > 0){
+                hash.put(nums2[j],hash.get(nums2[j]) - 1);
+                results.add(nums2[j]);
+            }
+        }
+        int[] result = new int[results.size()];
+        for(int i = 0; i < results.size(); i++){
             result[i] = results.get(i);
-
+        }
         return result;
-    }
+        }
